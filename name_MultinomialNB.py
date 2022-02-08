b33ca5f896ebb5b4a2be41e056f2b6d277e1af13
Johnsun <1079656279@qq.com>
2022-02-08 22:06:42 +0800

    添加了使用“name”和“address”两种特征cat，模型使用朴素贝叶斯的实验。

--- name_MultinomialNB.py
+++ name_MultinomialNB.py
@@ -1,11 +1,13 @@
 import jieba
 import pandas as pd
+import sklearn
 import torch
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, roc_curve, auc, average_precision_score, precision_recall_curve, \
     precision_score, recall_score, cohen_kappa_score
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import OneHotEncoder
 from torch.utils.data import DataLoader, TensorDataset
 
 # 这里是只使用了“name”这个特征，模型使用了朴素贝叶斯
@@ -13,6 +15,14 @@ from torch.utils.data import DataLoader, TensorDataset
 data = pd.read_csv('./data/POIs_dataset_test.csv')
 feature_name = data['name'].values
 labels = data['type'].values
+
+# -----这是将label变成one-hot的代码-----
+# labels = labels.reshape(-1, 1)
+# enc = OneHotEncoder()
+# labels = enc.fit_transform(labels).toarray()
+# labels = torch.Tensor(labels)
+# print(labels)
+# -----------------------------------
 
 tokenized_list = []
 for text in feature_name:
@@ -32,13 +42,13 @@ clf.fit(feature_name_train,labels_train)
 y_pred = clf.predict(feature_name_test)
 y_true = labels_test
 
-# ROC画图
-fpr, tpr, threshold = roc_curve(y_true, y_pred)  ###计算真正率和假正率
-roc_auc = auc(fpr, tpr)
-print(roc_auc)
-# PRC画图
-precision, recall, thresholds = precision_recall_curve(y_true, y_pred, pos_label=1)
-AP = average_precision_score(y_true, y_pred, average='macro', pos_label=1, sample_weight=None)
+# # ROC画图
+# fpr, tpr, threshold = roc_curve(y_true, y_pred)  ###计算真正率和假正率
+# roc_auc = auc(fpr, tpr)
+# print(roc_auc)
+# # PRC画图
+# precision, recall, thresholds = precision_recall_curve(y_true, y_pred, pos_label=1)
+# AP = average_precision_score(y_true, y_pred, average='macro', pos_label=1, sample_weight=None)
 
 # [fpr, tpr, roc_auc], [recall, precision, AP]
 
--- name_address_MultinomialNB.py
+++ name_address_MultinomialNB.py
@@ -2,41 +2,54 @@ import jieba
 import pandas as pd
 import torch
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import accuracy_score, roc_curve, auc, precision_recall_curve, average_precision_score, \
+from sklearn.metrics import accuracy_score, roc_curve, auc, average_precision_score, precision_recall_curve, \
     precision_score, recall_score, cohen_kappa_score
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from torch.utils.data import DataLoader, TensorDataset
 
-# 这里是只使用了“address”这个特征，模型使用了朴素贝叶斯
+# 这里是使用了“name”和“address”两个特征，模型使用了朴素贝叶斯
 
 data = pd.read_csv('./data/POIs_dataset_test.csv')
+feature_name = data['name'].values
 feature_address = data['address'].values
 labels = data['type'].values
 
-tokenized_list = []
+tokenized_list1 = []
+for text in feature_name:
+    words = [word for word in jieba.cut(text)]
+    tokenized_list1.append(' '.join(words))
+
+counter1 = CountVectorizer()
+counts1 = counter1.fit_transform(tokenized_list1)
+feature_name = torch.Tensor(counts1.toarray())
+
+tokenized_list2 = []
 for text in feature_address:
     words = [word for word in jieba.cut(text)]
-    tokenized_list.append(' '.join(words))
+    tokenized_list2.append(' '.join(words))
 
-counter = CountVectorizer()
-counts = counter.fit_transform(tokenized_list)
-feature_address = torch.Tensor(counts.toarray())
+counter2 = CountVectorizer()
+counts2 = counter2.fit_transform(tokenized_list1)
+feature_address = torch.Tensor(counts2.toarray())
 
-feature_address_train, feature_address_test, labels_train, labels_test = train_test_split(
-    feature_address, labels, test_size=0.15, random_state=66, stratify=labels)
+# 按dim=1才是每个向量的首尾拼接
+feature = torch.cat((feature_name, feature_address), dim=1)
+
+feature_train, feature_test, labels_train, labels_test = train_test_split(
+    feature, labels, test_size=0.15, random_state=66, stratify=labels)
 
 clf = MultinomialNB()
-clf.fit(feature_address_train,labels_train)
+clf.fit(feature_train, labels_train)
 
-y_pred = clf.predict(feature_address_test)
+y_pred = clf.predict(feature_test)
 y_true = labels_test
 
-# # ROC画图
+# ROC画图
 # fpr, tpr, threshold = roc_curve(y_true, y_pred)  ###计算真正率和假正率
-# roc_auc = auc(fpr,tpr)
+# roc_auc = auc(fpr, tpr)
 # print(roc_auc)
-# # PRC画图
+# PRC画图
 # precision, recall, thresholds = precision_recall_curve(y_true, y_pred, pos_label=1)
 # AP = average_precision_score(y_true, y_pred, average='macro', pos_label=1, sample_weight=None)
 
@@ -52,3 +65,4 @@ print('准确率：', AccuracyScore)
 print('召回率：', RecallScore)
 print('F1-score:', F1Score)
 print('KappaScore:', KappaScore)
+
